e2d0032105a50e0acc338b63febf46d9f73245c2
aalyia <44578252+aalyia@users.noreply.github.com>
2019-01-03 13:20:11 +0500

    Create script.js
    added a home page after the login ,student services will be provided after ownwards.

---
+++ script/script.js
@@ -0,0 +1,17 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function openNav() {
+  document.getElementById("mySidenav2").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav2").style.width = "0";
+}
+
+   
